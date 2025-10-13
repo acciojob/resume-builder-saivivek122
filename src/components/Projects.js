076a0fec0e5ca5d4cc1a79b92fceb3eb08efaf2f
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-10-13 07:57:00 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { addProject, deleteProject } from "./actions";
 
 const Projects = () => {
@@ -7,10 +7,7 @@ const Projects = () => {
   const projects = useSelector(state => state.projects);
   const [form, setForm] = useState({ projectName: "", techStack: "", description: "" });
 
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
   const handleAdd = () => {
     dispatch(addProject(form));
     setForm({ projectName: "", techStack: "", description: "" });
@@ -18,19 +15,14 @@ const Projects = () => {
 
   return (
     <div>
-      <h2>Add your Mini Projects</h2>
-      <input name="projectName" placeholder="Project Name" value={form.projectName} onChange={handleChange} />
-      <input name="techStack" placeholder="Tech Stack" value={form.techStack} onChange={handleChange} />
-      <input name="description" placeholder="Description" value={form.description} onChange={handleChange} />
-      <button id="next" onClick={handleAdd}>Add Project</button>
+      <h2>Mini Projects</h2>
+      <input name="projectName" data-testid="projectName-input" placeholder="Project Name" value={form.projectName} onChange={handleChange} />
+      <input name="techStack" data-testid="techStack-input" placeholder="Tech Stack" value={form.techStack} onChange={handleChange} />
+      <input name="description" data-testid="description-input" placeholder="Description" value={form.description} onChange={handleChange} />
+      <button id="add_project" onClick={handleAdd}>Add Project</button>
 
       <ul>
-        {projects.map((p, i) => (
-          <li key={i}>
-            {p.projectName} - {p.techStack}: {p.description}
-            <button id="delete" onClick={() => dispatch(deleteProject(i))}>Delete</button>
-          </li>
-        ))}
+        {projects.map((p,i) => <li key={i}>{p.projectName} - {p.techStack}: {p.description} <button id="delete" onClick={() => dispatch(deleteProject(i))}>Delete</button></li>)}
       </ul>
     </div>
   );
