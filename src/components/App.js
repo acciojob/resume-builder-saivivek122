0266a8a47e2224fbff3c034074bb3cf201709daa
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-10-13 07:18:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import { Provider } from "react-redux";
+import { store } from "./resumeSlice";
+import Profile from "./Profile";
+import Education from "./Education";
+import Skills from "./Skills";
+import Projects from "./Projects";
+import SocialMedia from "./SocialMedia";
+import ResumePreview from "./ResumePreview";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [page, setPage] = useState(1);
 
-export default App
+  const nextPage = () => setPage((prev) => Math.min(prev + 1, 6));
+  const prevPage = () => setPage((prev) => Math.max(prev - 1, 1));
+
+  return (
+    <Provider store={store}>
+      <div>
+        {page === 1 && <Profile />}
+        {page === 2 && <Education />}
+        {page === 3 && <Skills />}
+        {page === 4 && <Projects />}
+        {page === 5 && <SocialMedia />}
+        {page === 6 && <ResumePreview />}
+
+        <div style={{ marginTop: 20 }}>
+          {page > 1 && <button onClick={prevPage}>Back</button>}
+          {page < 6 && <button onClick={nextPage}>Next</button>}
+        </div>
+      </div>
+    </Provider>
+  );
+};
+
+export default App;
