45304251a7a8ff3e987ea6ae8a9c5b3ca7db8b4f
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-10-13 08:24:31 +0530

    Update Education.js

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,41 +1,68 @@
 import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { addEducation, deleteEducation } from "./actions";
 
-const Education = () => {
-  const dispatch = useDispatch();
-  const education = useSelector(state => state.education);
+function Education() {
+  const [educations, setEducations] = useState([]);
+  const [courseName, setCourseName] = useState("");
+  const [completionYear, setCompletionYear] = useState("");
+  const [college, setCollege] = useState("");
+  const [percentage, setPercentage] = useState("");
 
-  const [form, setForm] = useState({ courseName: "", completionYear: "", college: "", percentage: "" });
+  const handleAddEducation = () => {
+    setEducations([...educations, { courseName, completionYear, college, percentage }]);
+    setCourseName(""); setCompletionYear(""); setCollege(""); setPercentage("");
+  };
 
-  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
-  const handleAdd = () => {
-    dispatch(addEducation(form));
-    setForm({ courseName: "", completionYear: "", college: "", percentage: "" });
+  const handleDelete = (index) => {
+    setEducations(educations.filter((_, i) => i !== index));
   };
 
   return (
     <div>
       <h2>Add your Education Details</h2>
-      <input name="courseName" data-testid="courseName-input" placeholder="Course Name" value={form.courseName} onChange={handleChange} />
-      <input name="completionYear" data-testid="completionYear-input" placeholder="Completion Year" value={form.completionYear} onChange={handleChange} />
-      <input name="college" data-testid="college-input" placeholder="College" value={form.college} onChange={handleChange} />
-      <input name="percentage" data-testid="percentage-input" placeholder="Percentage" value={form.percentage} onChange={handleChange} />
-     <button
-  id="add_education"
-  className="MuiButton-contained"
-  onClick={handleAdd}
->
-  Add Education
-</button>
+      <input
+        id="courseName"
+        name="courseName"
+        placeholder="Course Name"
+        value={courseName}
+        onChange={e => setCourseName(e.target.value)}
+        data-testid="education-course"
+      />
+      <input
+        id="completionYear"
+        name="completionYear"
+        placeholder="Completion Year"
+        value={completionYear}
+        onChange={e => setCompletionYear(e.target.value)}
+        data-testid="education-year"
+      />
+      <input
+        id="college"
+        name="college"
+        placeholder="College Name"
+        value={college}
+        onChange={e => setCollege(e.target.value)}
+        data-testid="education-college"
+      />
+      <input
+        id="percentage"
+        name="percentage"
+        placeholder="Percentage"
+        value={percentage}
+        onChange={e => setPercentage(e.target.value)}
+        data-testid="education-percentage"
+      />
+      <button id="add_education" onClick={handleAddEducation}>Add Education</button>
 
       <ul>
-        {education.map((edu, i) => (
-          <li key={i}>{edu.courseName} - {edu.college} ({edu.completionYear}) [{edu.percentage}%] <button id="delete" onClick={() => dispatch(deleteEducation(i))}>Delete</button></li>
+        {educations.map((edu, index) => (
+          <li key={index}>
+            {edu.courseName} - {edu.college} ({edu.completionYear}) - {edu.percentage}%
+            <button id={`delete_education_${index}`} onClick={() => handleDelete(index)}>Delete</button>
+          </li>
         ))}
       </ul>
     </div>
   );
-};
+}
 
 export default Education;
