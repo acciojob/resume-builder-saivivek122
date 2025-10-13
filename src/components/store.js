105c3b2b69caf41f2f9c31f40ef75d6cb7157ba2
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-10-13 07:57:33 +0530

    Update store.js

--- src/components/store.js
+++ src/components/store.js
@@ -1,7 +1,54 @@
 import { createStore } from "redux";
-import { reducer } from "./reducers";
 
-export const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// Initial state
+const initialState = {
+  currentPage: 1,
+  profile: {},
+  education: [],
+  skills: [],
+  projects: [],
+  social: []
+};
+
+// Reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    // Navigation
+    case "NEXT_PAGE":
+      return { ...state, currentPage: state.currentPage + 1 };
+    case "PREV_PAGE":
+      return { ...state, currentPage: state.currentPage - 1 };
+
+    // Profile
+    case "SET_PROFILE":
+      return { ...state, profile: action.payload };
+
+    // Education
+    case "ADD_EDUCATION":
+      return { ...state, education: [...state.education, action.payload] };
+    case "DELETE_EDUCATION":
+      return { ...state, education: state.education.filter((_, i) => i !== action.payload) };
+
+    // Skills
+    case "ADD_SKILL":
+      return { ...state, skills: [...state.skills, action.payload] };
+    case "DELETE_SKILL":
+      return { ...state, skills: state.skills.filter((_, i) => i !== action.payload) };
+
+    // Projects
+    case "ADD_PROJECT":
+      return { ...state, projects: [...state.projects, action.payload] };
+    case "DELETE_PROJECT":
+      return { ...state, projects: state.projects.filter((_, i) => i !== action.payload) };
+
+    // Social
+    case "ADD_SOCIAL":
+      return { ...state, social: [...state.social, action.payload] };
+
+    default:
+      return state;
+  }
+};
+
+// Create store
+export const store = createStore(reducer);
