9a2d61df1f838658eaaf7a73199ba239392fa027
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-10-13 07:56:18 +0530

    Update Education.js

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,22 +1,14 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { addEducation, deleteEducation } from "./actions";
 
 const Education = () => {
   const dispatch = useDispatch();
   const education = useSelector(state => state.education);
 
-  const [form, setForm] = useState({
-    courseName: "",
-    completionYear: "",
-    college: "",
-    percentage: ""
-  });
+  const [form, setForm] = useState({ courseName: "", completionYear: "", college: "", percentage: "" });
 
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
-
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
   const handleAdd = () => {
     dispatch(addEducation(form));
     setForm({ courseName: "", completionYear: "", college: "", percentage: "" });
@@ -24,19 +16,16 @@ const Education = () => {
 
   return (
     <div>
-      <h2>Add your Education Details</h2>
-      <input name="courseName" placeholder="Course Name" value={form.courseName} onChange={handleChange} />
-      <input name="completionYear" placeholder="Completion Year" value={form.completionYear} onChange={handleChange} />
-      <input name="college" placeholder="College" value={form.college} onChange={handleChange} />
-      <input name="percentage" placeholder="Percentage" value={form.percentage} onChange={handleChange} />
+      <h2>Education</h2>
+      <input name="courseName" data-testid="courseName-input" placeholder="Course Name" value={form.courseName} onChange={handleChange} />
+      <input name="completionYear" data-testid="completionYear-input" placeholder="Completion Year" value={form.completionYear} onChange={handleChange} />
+      <input name="college" data-testid="college-input" placeholder="College" value={form.college} onChange={handleChange} />
+      <input name="percentage" data-testid="percentage-input" placeholder="Percentage" value={form.percentage} onChange={handleChange} />
       <button id="add_education" onClick={handleAdd}>Add Education</button>
 
       <ul>
         {education.map((edu, i) => (
-          <li key={i}>
-            {edu.courseName} - {edu.college} ({edu.completionYear}) [{edu.percentage}%]
-            <button id="delete" onClick={() => dispatch(deleteEducation(i))}>Delete</button>
-          </li>
+          <li key={i}>{edu.courseName} - {edu.college} ({edu.completionYear}) [{edu.percentage}%] <button id="delete" onClick={() => dispatch(deleteEducation(i))}>Delete</button></li>
         ))}
       </ul>
     </div>
